9847f8a7ce1c69acf1759535a968190df704fdec
Sergey Panteleenko <depstor90@gmail.com>
2026-04-04 01:37:33 +0300

    ref

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,7 +1,9 @@
-import './charList.scss';
+import {Component} from 'react';
+import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
-import { Component } from 'react';
 import MarvelService from '../../services/MarvelService';
+import './charList.scss';
 
 class CharList extends Component {
 
@@ -10,7 +12,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 5,
+        offset: 210,
         charEnded: false
     }
     
@@ -35,7 +37,7 @@ class CharList extends Component {
 
     onCharListLoaded = (newCharList) => {
         let ended = false;
-        if (newCharList.length < 5) {
+        if (newCharList.length < 9) {
             ended = true;
         }
 
@@ -43,7 +45,7 @@ class CharList extends Component {
             charList: [...charList, ...newCharList],
             loading: false,
             newItemLoading: false,
-            offset: offset + 5,
+            offset: offset + 9,
             charEnded: ended
         }))
     }
@@ -55,8 +57,21 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
+
     renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -65,14 +80,20 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
             )
         });
-
+        // А эта конструкция вынесена для центровки спиннера/ошибки
         return (
             <ul className="char__grid">
                 {items}
@@ -86,11 +107,13 @@ class CharList extends Component {
         
         const items = this.renderItems(charList);
 
+
         const spinner = loading ? <Spinner/> : null;
         const content = !(loading || error) ? items : null;
 
         return (
             <div className="char__list">
+
                 {spinner}
                 {content}
                 <button 
@@ -103,6 +126,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
